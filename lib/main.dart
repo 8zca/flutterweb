f013b568e9154257aba9a97c681abfb8be33cb38
Moriwaki Kazuya <kazuya.m@sora.flights>
2019-08-04 14:36:27 +0900

    login view

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.teal,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Dashboard Login'),
     );
   }
 }
@@ -23,39 +23,72 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Center(
+          child: Text(
+            title,
+            textAlign: TextAlign.center,
+          ),
+        ),
+        backgroundColor: Colors.cyan,
+        elevation: 0        
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (choose the "Toggle Debug Paint" action
-          // from the Flutter Inspector in Android Studio, or the "Toggle Debug
-          // Paint" command in Visual Studio Code) to see the wireframe for each
-          // widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'Hello, World!',
+            Container(
+              width: 160,
+              height: 150,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  image: NetworkImage('https://images.unsplash.com/photo-1563699498778-aa8246fa5456?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2552&q=80'),
+                  fit: BoxFit.cover
+                )
+              ),
+              margin: EdgeInsets.only(bottom: 48),
             ),
-          ],
-        ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+            Card(
+              child: Container(
+                padding: EdgeInsets.all(48),
+                width: 480,
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Email'
+                        ),
+                      ),
+                      padding: EdgeInsets.only(bottom: 24)
+                    ),
+                    Container(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          labelText: 'Password'
+                        ),
+                      ),
+                      padding: EdgeInsets.only(bottom: 24),
+                    ),
+                    Container(
+                      child: RaisedButton(
+                        onPressed: () {},
+                        child: Text('Login', style: TextStyle(color: Colors.white)),
+                        elevation: 0,
+                        highlightElevation: 0,
+                        hoverElevation: 0.1,
+                        color: Colors.cyan,
+                      )
+                    )
+                  ]
+                )
+              )
+            )
+          ]
+        )
+      ) // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
